4eeda86901995de16f160430bb7beae31160b3de
Kukuh Alfian <hi@kalfian.com>
2021-06-20 11:23:28 +0700

    [ADD] Midtrans Middleware

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -47,4 +47,24 @@ class Controller extends BaseController
         }
         return false;
     }
+
+    /**
+     * Show the specified resource.
+     * @param Property $property
+     * @return Renderable
+     */
+    public function getMidtrans($params) {
+        // Set your Merchant Server Key
+        \Midtrans\Config::$serverKey = env('MIDTRANS_SERVER_KEY');
+        // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
+        \Midtrans\Config::$isProduction = (bool)env('MIDTRANS_PRODUCTION');
+        // Set sanitization on (default)
+        \Midtrans\Config::$isSanitized = (bool)env('MIDTRANS_SANITIZED');
+        // Set 3DS transaction for credit card to true
+        \Midtrans\Config::$is3ds = (bool)env('MIDTRANS_3DS');
+        
+        $snapToken = \Midtrans\Snap::createTransaction($params);
+
+        return $snapToken;
+    }
 }
